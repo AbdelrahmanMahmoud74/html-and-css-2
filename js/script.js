b8fbab61d311c70c2e362545b69f532451a293ae
AbdelrahmanMahmoud74 <133714220+AbdelrahmanMahmoud74@users.noreply.github.com>
2023-09-21 23:56:53 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+// document.getElementById("btn1").onclick=function(){
+//     document.getElementById("div1").innerHTML=("u succeded on the test")
+
+// }
+// console.log("محمد رسول الله")
+/*  */
+// alert("خلي بالك يسط")
+// confirm("انت عاوز تسجل يابا")
+// prompt("enta norm ? ya or na")
+// var num1;
+// var num2;
+// prompt(num1,num2);
+// console.log(num1+num2);
